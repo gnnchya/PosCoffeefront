5081d6919744cb8b8e6048ec82ac303a5cab7a35
pondnarawich <pondnarawich@gmail.com>
2021-07-22 18:06:30 +0700

    -

--- tryReact/my-app/src/pages/homePage.js
+++ tryReact/my-app/src/pages/homePage.js
@@ -7,7 +7,7 @@ function HomePage(){
             <div className="v6_3"></div>
             <div className="v6_2"></div>
             <div className="name"></div>
-            <Link to="/history"><div className="v6_10">
+            <Link to="/showCart"><div className="v6_10">
                 <div className="v6_9"></div>
                 <span className="v6_8">cart</span>
             </div></Link>
@@ -16,13 +16,13 @@ function HomePage(){
             {/*<form action="https://google.com">*/}
             {/*    <input type="submit" value="Go to Google" />*/}
             {/*</form>*/}
-            <Link to="/history"><div className="v6_14">MENU</div></Link>
-            <Link to="/order"><span className="v6_15">ORDER</span></Link>
-            <Link to="/money"><span className="v6_19">MONEY</span></Link>
-            <Link to="/stock"><span className="v6_20">STOCK</span></Link>
+            <Link to="/showMenu"><div className="v6_14">MENU</div></Link>
+            <Link to="/showOrder"><span className="v6_15">ORDER</span></Link>
+            <Link to="/showMoney"><span className="v6_19">MONEY</span></Link>
+            <Link to="/showStock"><span className="v6_20">STOCK</span></Link>
             <span className="v6_16">ADMIN</span>
-            <Link to="/report"><span className="v6_17">REPORT</span></Link>
-            <Link to="/history"><span className="v6_18">HISTORY</span></Link>
+            <Link to="/showReport"><span className="v6_17">REPORT</span></Link>
+            <Link to="/showHistory"><span className="v6_18">HISTORY</span></Link>
             <span className="v6_25">
                 <div className="v6_22"></div>
                 <span className="v6_23">LOG OUT</span>
