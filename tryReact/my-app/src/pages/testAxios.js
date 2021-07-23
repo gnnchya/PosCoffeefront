ef18d7bc541199ac5da4f49a63bfcb3995a3d7c8
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-23 15:27:47 +0700

    Update testAxios.js

--- tryReact/my-app/src/pages/testAxios.js
+++ tryReact/my-app/src/pages/testAxios.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React from 'react';
 import axios from 'axios'
+
+
 function TestAxios() {
-    var menu = [];
-    axios.get("http://127.0.0.1:8080/pos/menu").then(res => {
-        alert(String(res.data.status))
-        console.log(res);
-        menu = res.data
+    const getdata = document.getElementById('data');
+    const getId = document.getElementById('id');
+    const getAvailable= document.getElementById('available');
+    const getCategory = document.getElementById('category');
+    const getIngredients = document.getElementById('ingredient');
+    const getName = document.getElementById('name');
+    const getPrice = document.getElementById('price');
+
+
+    axios.get("http://127.0.0.1:8080/pos/menu/c3e2obiciaeng9b27p3g").then(res => {
+        console.log(res.data.data);
+        console.log(res.data.data.id);
+
+        getId.innerHTML = res.data.data.id;
+        getName.innerHTML = res.data.data.name;
+        getCategory.innerHTML = res.data.data.category;
+        getIngredients.innerHTML = res.data.data.ingredient;
+        getPrice.innerHTML = res.data.data.getPrice;
+        getAvailable.innerHTML = res.data.data.avalable;
     });
+    
 
-    return(
-        <h2>
-            {String(menu.id)}
-        </h2>
-    )
-}
-
+        return(
+            <div>
+                <h2 id="id"></h2>
+            </div>
+        )
+        }
 export default TestAxios
