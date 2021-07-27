3a0047a86640cea9cb9fe7c85630ccc547cc67d6
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-27 14:56:43 +0700

    -

--- tryReact/my-app/src/pages/showOrder.js
+++ tryReact/my-app/src/pages/showOrder.js
@@ -1,144 +1,70 @@
-import React from "react"
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+import { getAllOrder } from '../actions/posts'
 
-function ShowCart(){
-    return(
-        <div class="v17_232">
-        <div class="v17_233"></div>
-        <div class="v17_234"></div>
-        <div class="name"></div>
-        <div class="v17_236">
-            <div class="v17_237"></div><span class="v17_238">cart
+function ShowOrder() {
+    const [orderItem, setOrderItems] = useState([])
+    // const [menu, setMenu] = useState({})
+    useEffect(() => {
+        getList()
+    }, [])
+
+    const getList = async (e) => {
+        try {
+            const response = await getAllOrder()
+            console.log(response.data.data)
+            // alert(response.data.data[0])
+            if (response.status === 200) {
+                setOrderItems(response.data.data || [])
+            }
+        } catch (error) {
+            alert(error)
+        }
+    }
+
+    return (
+        <div className="v1_3">
+            <div className="v6_3"></div>
+            <div className="v6_2"></div>
+            <Link to="/showCart"><div className="v6_10">
+                <div className="v6_9"></div>
+                <span className="v6_8">cart</span>
+            </div></Link>
+            <div className="v6_12"></div>
+            <span className="v6_13">SHOP</span>
+            {/*<form action="https://google.com">*/}
+            {/*    <input type="submit" value="Go to Google" />*/}
+            {/*</form>*/}
+            <Link to="/showMenu"><div className="v6_14">MENU</div></Link>
+            <Link to="/showOrder"><span className="v6_15">ORDER</span></Link>
+            <Link to="/showMoney"><span className="v6_19">MONEY</span></Link>
+            <Link to="/showStock"><span className="v6_20">STOCK</span></Link>
+            <span className="v6_16">ADMIN</span>
+            <Link to="/showReport"><span className="v6_17">REPORT</span></Link>
+            <Link to="/showHistory"><span className="v6_18">HISTORY</span></Link>
+            <span className="v6_25">
+                <div className="v6_22"></div>
+                <span className="v6_23">LOG OUT</span>
             </span>
+            <Link to="/homepage"><span className="v6_32">POS COFFEE</span></Link>
+            <div className="pagination">
+                <a href="#">❮</a>
+                <a href="#">❯</a>
+            </div>
+            <div className='menu'>
+            {orderItem.map((item, index) => {
+                return (
+                    
+                        <Link to={{pathname:`/menuInfo/${item.id}`}}><div className="v12_301">
+                            <div className="v6_167"></div>
+                            <div className="v6_168"></div><span className="v6_169" key={index}>{item.name}</span><span className="v6_170" key={index}>{item.price}</span>
+                        </div></Link>
+                    
+                )
+            })}
+           </div>
         </div>
-        <div class="v17_239"></div><span class="v17_240">SHOP</span>
-        <div class="v17_241"></div><span class="v17_242">MENU</span><span class="v17_243">🛒</span><span
-            class="v17_244">ORDER</span><span class="v17_245">MONEY</span><span class="v17_246">STOCK</span><span
-            class="v17_247">ADMIN</span><span class="v17_248">REPORT</span><span class="v17_249">HISTORY</span>
-        <div class="v17_250">
-            <div class="v17_251"></div><span class="v17_252">LOG OUT</span>
-        </div>
-        <div class="v17_256"></div><span class="v17_257">POS COFEE</span>
-        <div class="v17_258">
-            <div class="v17_259"></div>
-            <div class="name"></div>
-        </div>
-        <div class="v17_261">
-            <div class="v17_262"></div>
-            <div class="name"></div>
-        </div>
-        <div class="v17_264"></div><span class="v17_265">1 2 3 4 5 6 7</span>
-        <div class="v17_353">
-            <div class="v17_266">
-                <div class="v17_267"></div>
-                <div class="v17_268"></div><span class="v17_269">ICED LATTE</span><span class="v17_270">50 $</span>
-                <div class="v17_271"></div>
-                <div class="v17_272"></div><span class="v17_273">ORDER NO.1</span><span class="v17_274">ICED
-                    LATTE</span>
-            </div><span class="v17_352">50 $</span>
-        </div>
-        <div class="v17_354">
-            <div class="v17_355">
-                <div class="v17_356"></div>
-                <div class="v17_357"></div><span class="v17_358">ICED LATTE</span><span class="v17_359">50 $</span>
-                <div class="v17_360"></div>
-                <div class="v17_361"></div><span class="v17_362">ORDER NO.1</span><span class="v17_363">ICED
-                    LATTE</span>
-            </div><span class="v17_364">50 $</span>
-        </div>
-        <div class="v17_367">
-            <div class="v17_368">
-                <div class="v17_369"></div>
-                <div class="v17_370"></div><span class="v17_371">ICED LATTE</span><span class="v17_372">50 $</span>
-                <div class="v17_373"></div>
-                <div class="v17_374"></div><span class="v17_375">ORDER NO.1</span><span class="v17_376">ICED
-                    LATTE</span>
-            </div><span class="v17_377">50 $</span>
-        </div>
-        <div class="v17_378">
-            <div class="v17_379">
-                <div class="v17_380"></div>
-                <div class="v17_381"></div><span class="v17_382">ICED LATTE</span><span class="v17_383">50 $</span>
-                <div class="v17_384"></div>
-                <div class="v17_385"></div><span class="v17_386">ORDER NO.1</span><span class="v17_387">ICED
-                    LATTE</span>
-            </div><span class="v17_388">50 $</span>
-        </div><span class="v17_389">ICED LATTE</span><span class="v17_390">50 $</span>
-        <div class="v17_391">
-            <div class="v17_392">
-                <div class="v17_393"></div>
-                <div class="v17_394"></div><span class="v17_395">ICED LATTE</span><span class="v17_396">50 $</span>
-                <div class="v17_397"></div>
-                <div class="v17_398"></div><span class="v17_399">ORDER NO.1</span><span class="v17_400">ICED
-                    LATTE</span>
-            </div><span class="v17_401">50 $</span>
-        </div>
-        <div class="v17_402">
-            <div class="v17_403">
-                <div class="v17_404"></div>
-                <div class="v17_405"></div><span class="v17_406">ICED LATTE</span><span class="v17_407">50 $</span>
-                <div class="v17_408"></div>
-                <div class="v17_409"></div><span class="v17_410">ORDER NO.1</span><span class="v17_411">ICED
-                    LATTE</span>
-            </div><span class="v17_412">50 $</span>
-        </div><span class="v17_413">ICED LATTE</span><span class="v17_414">50 $</span>
-        <div class="v17_415">
-            <div class="v17_416">
-                <div class="v17_417"></div>
-                <div class="v17_418"></div><span class="v17_419">ICED LATTE</span><span class="v17_420">50 $</span>
-                <div class="v17_421"></div>
-                <div class="v17_422"></div><span class="v17_423">ORDER NO.1</span><span class="v17_424">ICED
-                    LATTE</span>
-            </div><span class="v17_425">50 $</span>
-        </div>
-        <div class="v17_426">
-            <div class="v17_427">
-                <div class="v17_428"></div>
-                <div class="v17_429"></div><span class="v17_430">ICED LATTE</span><span class="v17_431">50 $</span>
-                <div class="v17_432"></div>
-                <div class="v17_433"></div><span class="v17_434">ORDER NO.1</span><span class="v17_435">ICED
-                    LATTE</span>
-            </div><span class="v17_436">50 $</span>
-        </div><span class="v17_437">ICED LATTE</span><span class="v17_438">50 $</span>
-        <div class="v17_439">
-            <div class="v17_440">
-                <div class="v17_441"></div>
-                <div class="v17_442"></div><span class="v17_443">ICED LATTE</span><span class="v17_444">50 $</span>
-                <div class="v17_445"></div>
-                <div class="v17_446"></div><span class="v17_447">ORDER NO.1</span><span class="v17_448">ICED
-                    LATTE</span>
-            </div><span class="v17_449">50 $</span>
-        </div>
-        <div class="v17_450">
-            <div class="v17_451">
-                <div class="v17_452"></div>
-                <div class="v17_453"></div><span class="v17_454">ICED LATTE</span><span class="v17_455">50 $</span>
-                <div class="v17_456"></div>
-                <div class="v17_457"></div><span class="v17_458">ORDER NO.1</span><span class="v17_459">ICED
-                    LATTE</span>
-            </div><span class="v17_460">50 $</span>
-        </div><span class="v17_461">ICED LATTE</span><span class="v17_462">50 $</span>
-        <div class="v17_463">
-            <div class="v17_464">
-                <div class="v17_465"></div>
-                <div class="v17_466"></div><span class="v17_467">ICED LATTE</span><span class="v17_468">50 $</span>
-                <div class="v17_469"></div>
-                <div class="v17_470"></div><span class="v17_471">ORDER NO.1</span><span class="v17_472">ICED
-                    LATTE</span>
-            </div><span class="v17_473">50 $</span>
-        </div>
-        <div class="v17_474">
-            <div class="v17_475">
-                <div class="v17_476"></div>
-                <div class="v17_477"></div><span class="v17_478">ICED LATTE</span><span class="v17_479">50 $</span>
-                <div class="v17_480"></div>
-                <div class="v17_481"></div><span class="v17_482">ORDER NO.1</span><span class="v17_483">ICED
-                    LATTE</span>
-            </div><span class="v17_484">50 $</span>
-        </div>
-    </div>
-    
     )
 }
 
-export default ShowCart
+export default ShowOrder
