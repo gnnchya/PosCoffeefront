d67c8086f1dbf19fec561d57e3c2fbe780c4e037
gnnchya <75212443+gnnchya@users.noreply.github.com>
2021-07-27 12:05:49 +0700

    Update showCart.js

--- tryReact/my-app/src/pages/showCart.js
+++ tryReact/my-app/src/pages/showCart.js
@@ -11,9 +11,6 @@ function ShowCart(){
             </div></Link>
             <div className="v6_12"></div>
             <span className="v6_13">SHOP</span>
-            {/*<form action="https://google.com">*/}
-            {/*    <input type="submit" value="Go to Google" />*/}
-            {/*</form>*/}
             <Link to="/showMenu"><div className="v6_14">MENU</div></Link>
             <Link to="/showOrder"><span className="v6_15">ORDER</span></Link>
             <Link to="/showMoney"><span className="v6_19">MONEY</span></Link>
@@ -26,34 +23,34 @@ function ShowCart(){
                 <span className="v6_23">LOG OUT</span>
             </span>
             
-        <Link to="/homepage"><span className="v6_32">POS COFFEE</span></Link>
-        
-        <span class="v14_84">Name : </span>
-        <div class="v14_85"></div><span class="v14_86">Description :</span>
-        <div class="v14_87"></div><span class="v17_112">Price :</span>
-        <div class="v17_113"></div><span class="v17_106">Amount :</span>
-        <div class="v17_107"></div>
-        <span class="v17_121">Name : </span>
-        <div class="v17_122"></div><span class="v17_123">Description :</span>
-        <div class="v17_124"></div><span class="v17_125">Price :</span>
-        <div class="v17_126"></div><span class="v17_127">Amount :</span>
-        <div class="v17_128"></div>
-        <div class="name"></div>
+            <Link to="/homepage"><span className="v6_32">POS COFFEE</span></Link>
+            
+            <span class="v14_84">Name : </span>
+            <div class="v14_85"></div><span class="v14_86">Description :</span>
+            <div class="v14_87"></div><span class="v17_112">Price :</span>
+            <div class="v17_113"></div><span class="v17_106">Amount :</span>
+            <div class="v17_107"></div>
+            <span class="v17_121">Name : </span>
+            <div class="v17_122"></div><span class="v17_123">Description :</span>
+            <div class="v17_124"></div><span class="v17_125">Price :</span>
+            <div class="v17_126"></div><span class="v17_127">Amount :</span>
+            <div class="v17_128"></div>
+            <div class="name"></div>
 
-        <div className="payment">
-            <span class="v17_136">Paid :</span>
-            <div class="v17_137"></div><span class="v17_138">Method :</span>
-            <div class="v17_139"></div><span class="v17_140">Type :</span>
-            <div class="v17_141"></div><span class="v17_144">Total Price :</span>
-            <div class="v17_145"></div><span class="v17_142">Destination :</span>
-            <div class="v17_143"></div>
+            <div className="payment">
+                <span class="v17_136">Paid :</span>
+                <div class="v17_137"></div><span class="v17_138">Method :</span>
+                <div class="v17_139"></div><span class="v17_140">Type :</span>
+                <div class="v17_141"></div><span class="v17_144">Total Price :</span>
+                <div class="v17_145"></div><span class="v17_142">Destination :</span>
+                <div class="v17_143"></div>
+            </div>
+
+            <div class="v14_81">
+                <span class="v14_82">Finish</span>
+            </div>
+
         </div>
-
-        <div class="v14_81">
-            <span class="v14_82">Finish</span>
-        </div>
-
-    </div>
 
     
     )
