95c8cccf5b79febd1f9fbcbdad1df4e12aef879e
pondnarawich <pondnarawich@gmail.com>
2021-07-27 14:15:41 +0700

    -

--- tryReact/my-app/src/pages/showMenu.js
+++ tryReact/my-app/src/pages/showMenu.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
-import { createPost, getMenu } from '../actions/posts'
+import { createPost, getAllMenu } from '../actions/posts'
 // import PostList from '../components/PostList'
 
 function ShowMenu() {
@@ -12,7 +12,7 @@ function ShowMenu() {
 
     const getList = async (e) => {
         try {
-            const response = await getMenu()
+            const response = await getAllMenu()
             console.log(response.data.data)
             // alert(response.data.data[0])
             if (response.status === 200) {
@@ -59,7 +59,7 @@ function ShowMenu() {
             {menuItem.map((item, index) => {
                 return (
                     
-                        <Link to="/menuInfo"><div className="v12_301">
+                        <Link to={{pathname:`/menuInfo/${item.id}`}}><div className="v12_301">
                             <div className="v6_167"></div>
                             <div className="v6_168"></div><span className="v6_169" key={index}>{item.name}</span><span className="v6_170" key={index}>{item.price}</span>
                         </div></Link>
